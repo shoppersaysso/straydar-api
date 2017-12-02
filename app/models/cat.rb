df223b379d76224e7dd6cf3b8281fdc9a44e9a48
Sara Hopper <sara.hopper@gmail.com>
2017-12-02 15:03:34 -0500

    syntax error fix for cloudinary

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,10 +3,11 @@ class Cat < ApplicationRecord
   after_validation :reverse_geocode
 
   has_attached_file :photo,
-  :styles => { :original => "300x300>", :thumb => "100x100>" },
-  :default_url => "assets/images/:style/missing.png"
   :storage => :cloudinary,
+  :styles => { :original => "300x300>", :thumb => "100x100>" },
+  :default_url => "assets/images/:style/missing.png",
   :path => ':id/:style:/:filename'
+  
   do_not_validate_attachment_file_type :photo
 
 
